caa022764912eea9ae8dd4deb1d0327db0d92e9f
莫小苝 <lovejing0306@gmail.com>
2016-10-14 21:46:32 +0800

    update SparkMLlib

---
+++ 04-ClassificationandRegression/cr_lsvm.py
@@ -0,0 +1,31 @@
+# coding=utf-8
+
+from pyspark import SparkContext, SparkConf
+from pyspark.mllib.classification import SVMWithSGD, SVMModel
+from pyspark.mllib.regression import LabeledPoint
+
+conf = SparkConf().setAppName('Linear Support Vector Machiens').setMaster('local[2]')
+sc = SparkContext(conf=conf)
+
+# load and parse the data
+def parsePoint(line):
+    values = [float(x) for x in line.split(' ')]
+    return LabeledPoint(values[0], values[1:])
+
+data = sc.textFile('../data/sample_svm_data.txt')
+parseData = data.map(parsePoint)
+
+# build the model
+model = SVMWithSGD.train(parseData, iterations=100)
+
+# evaluating the model on training data
+labelsAndPreds = parseData.map(lambda p : (p.label, model.predict(p.features)))
+trainErr = labelsAndPreds.filter(lambda (v, p) : v != p).count()/float(parseData.count())
+
+print('training error :' + str(trainErr))
+
+# save and load model
+model.save(sc, '../model/pythonSVMWithSGDModel')
+sameModel = SVMModel.load(sc, '../model/pythonSVMWithSGDModel')
+
+sc.stop()
