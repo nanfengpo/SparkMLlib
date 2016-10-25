48f4dd59235b467d9ce054894801fa8dc0cc3378
莫小苝 <lovejing0306@gmail.com>
2016-10-25 19:41:56 +0800

    create multiclassclassification.py

--- 10-EvaluationMetrics/multiclassclassification.py
+++ 10-EvaluationMetrics/multiclassclassification.py
@@ -2,13 +2,31 @@
 
 from pyspark import SparkContext, SparkConf
 from pyspark.mllib.classification import LogisticRegressionWithLBFGS
-from pyspark.mllib.evaluation import BinaryClassificationMetrics
+from pyspark.mllib.evaluation import MulticlassMetrics
 from pyspark.mllib.regression import LabeledPoint
 from pyspark.mllib.util import MLUtils
 
 conf = SparkConf().setAppName('Binary Classification Evaluation').setMaster('local[2]')
 sc = SparkContext(conf=conf)
 
+# load training data
+data = MLUtils.loadLibSVMFile(sc, '../data/sample_multiclass_classification_data.txt')
 
+# split data into training and test
+training, test = data.randomSplit([0.6, 0.4], seed=11)
+training.cache()
 
+# run training algorithm to build the model
+model = LogisticRegressionWithLBFGS.train(training, numClasses=3)
+
+# compute raw scores on the test set
+predictionAndLabels = test.map(lambda lp : (float(model.predict(lp.features)), lp.label))
+
+# instanticate metrics object
+metrics = MulticlassMetrics(predictionAndLabels)
+
+print('summary stats')
+print('precision= %s' % metrics.precision())
+print('recall= %s' % metrics.recall())
+print('F1 Score= %s' % metrics.fMeasure())
 sc.stop()
